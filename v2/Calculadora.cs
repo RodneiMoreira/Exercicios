9e629bb2c0412e0e8360ba33abf4bdf2bec9e48f
Rodnei Moreira <rodnei.moreira@mttechne.com.br>
2020-03-06 18:38:47 -0300

    v2.0

---
+++ v2/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿namespace NovaCalculadora
+{
+    public class Calculadora
+    {
+        public decimal somar(decimal num1, decimal num2)
+        {
+            decimal resultado = num1 + num2;
+            return resultado;
+        }
+        public decimal subtrair(decimal num1, decimal num2)
+        {
+            decimal resultado = num1 - num2;
+            return resultado;
+        }
+        public decimal multiplicar(decimal num1, decimal num2)
+        {
+            decimal resultado = num1 * num2;
+            return resultado;
+        }
+        public decimal dividir(decimal num1, decimal num2)
+        {
+            decimal resultado = num1 / num2;
+            return resultado;
+        }
+    
+        
+    
+    
+    }
+
+}
---
+++ v2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace NovaCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calculadora Acao");
+
+            Calculadora acao = new Calculadora();
+
+            Console.WriteLine("digite um número");
+
+            decimal num1 = Convert.ToDecimal(Console.ReadLine());
+
+            while (true)
+            {
+
+                Console.WriteLine("digite outro numero");
+
+                decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("escolha uma operaçao");
+
+                Console.WriteLine("A) para adição");
+                Console.WriteLine("B) para subtração");
+                Console.WriteLine("C) para multiplicação");
+                Console.WriteLine("D) para divisão");
+                Console.WriteLine("E) para terminar");
+
+                decimal resultado = 0;
+
+                string operadorEscolhido = Console.ReadLine();
+
+                if (operadorEscolhido == "A" || operadorEscolhido == "a")
+                {
+                    resultado = acao.somar(num1, num2);
+                }
+                else if (operadorEscolhido == "B" || operadorEscolhido == "b")
+                {
+                    resultado = acao.subtrair(num1, num2);
+                }
+                else if (operadorEscolhido == "C" || operadorEscolhido == "c")
+                {
+                    resultado = acao.multiplicar(num1, num2);
+                }
+                else if (operadorEscolhido == "D" || operadorEscolhido == "d")
+                {
+                    resultado = acao.dividir(num1, num2);
+                }
+                else if (operadorEscolhido == "E" || operadorEscolhido == "e")
+                {
+                    return;
+                }
+
+                Console.WriteLine("o resultado é");
+
+                Console.WriteLine(resultado);
+
+                Console.WriteLine("deseja continuar (S/N)?");
+
+                string escolha = Console.ReadLine();
+
+                if (escolha == "S" || escolha == "s")
+                {
+                    num1 = resultado;
+                }
+                else if (escolha == "N" || escolha == "n")
+                {
+                    Console.WriteLine("Obrigado por utilizar esta calculadora");
+
+                    Console.ReadKey();
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
